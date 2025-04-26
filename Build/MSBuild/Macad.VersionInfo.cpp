a8033f1ab491005a015afb5dab0bcb311e843d7e
dipts <dipts@users.noreply.github.com>
2025-04-26 21:19:41 +0200

    [Build] Raise version to 4.2 development.

--- Build/MSBuild/Macad.VersionInfo.cpp
+++ Build/MSBuild/Macad.VersionInfo.cpp
@@ -1,5 +1,5 @@
 using namespace System::Reflection;
-[assembly:AssemblyVersionAttribute("4.1.2043.0")];
+[assembly:AssemblyVersionAttribute("4.2.2045.3")];
 [assembly:AssemblyCopyrightAttribute(L"Copyright © 2015-2025 Tobias Schachte")];
 [assembly:AssemblyCompanyAttribute(L"Tobias Schachte")];
 [assembly:AssemblyProductAttribute(L"Macad|3D")];
