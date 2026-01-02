f67b25223fe9f35219e1d1f43d8eb19284f79be9
dipts <dipts@users.noreply.github.com>
2026-01-02 16:53:10 +0100

    [Build] Raised version to 4.4.2193

--- Build/MSBuild/Macad.VersionInfo.cpp
+++ Build/MSBuild/Macad.VersionInfo.cpp
@@ -1,5 +1,5 @@
 using namespace System::Reflection;
-[assembly:AssemblyVersionAttribute("4.3.2108.0")];
-[assembly:AssemblyCopyrightAttribute(L"Copyright © 2015-2025 Tobias Schachte")];
+[assembly:AssemblyVersionAttribute("4.4.2193.0")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright © 2015-2026 Tobias Schachte")];
 [assembly:AssemblyCompanyAttribute(L"Tobias Schachte")];
 [assembly:AssemblyProductAttribute(L"Macad|3D")];
