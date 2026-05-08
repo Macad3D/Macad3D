1de920f28539186a7f49ba39ddf46905d1ff8888
dipts <14055919+dipts@users.noreply.github.com>
2026-05-08 19:00:23 +0200

    [Styles] Updated AvalonDock to 4.74.1, adjusted styles and templates.

--- Build/MSBuild/Macad.VersionInfo.cpp
+++
@@ -1,5 +0,0 @@
-using namespace System::Reflection;
-[assembly:AssemblyVersionAttribute("5.0.2109.3")];
-[assembly:AssemblyCopyrightAttribute(L"Copyright © 2015-2025 Tobias Schachte")];
-[assembly:AssemblyCompanyAttribute(L"Tobias Schachte")];
-[assembly:AssemblyProductAttribute(L"Macad|3D")];
